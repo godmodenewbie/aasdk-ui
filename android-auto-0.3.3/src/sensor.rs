6eca9d78f183b8dfbc01ee6f301feff3d475f8ba
godmodenewbie <saputra.nobin@gmail.com>
2026-03-29 18:49:46 +0800

    feat: implement Android Auto protocol library with support for USB and wireless connections

---
+++ android-auto-0.3.3/src/sensor.rs
@@ -0,0 +1,165 @@
+//! Contains sensor channel code
+
+use super::{
+    AndroidAutoCommonMessage, AndroidAutoConfiguration, AndroidAutoFrame, ChannelDescriptor,
+    ChannelHandlerTrait, ChannelId, FrameHeader, FrameHeaderContents, FrameHeaderType,
+};
+use crate::{AndroidAutoMainTrait, StreamMux, Wifi};
+use protobuf::Message;
+
+/// A message about sensors in android auto
+#[derive(Debug)]
+pub enum SensorMessage {
+    /// A request to start a specific sensor
+    SensorStartRequest(ChannelId, Wifi::SensorStartRequestMessage),
+    /// A response to the sensor start request
+    SensorStartResponse(ChannelId, Wifi::SensorStartResponseMessage),
+    /// A message containing sensor data
+    Event(ChannelId, Wifi::SensorEventIndication),
+}
+
+impl From<SensorMessage> for AndroidAutoFrame {
+    fn from(value: SensorMessage) -> Self {
+        match value {
+            SensorMessage::SensorStartRequest(_, _) => todo!(),
+            SensorMessage::SensorStartResponse(chan, m) => {
+                let mut data = m.write_to_bytes().unwrap();
+                let t = Wifi::sensor_channel_message::Enum::SENSOR_START_RESPONSE as u16;
+                let t = t.to_be_bytes();
+                let mut m = Vec::new();
+                m.push(t[0]);
+                m.push(t[1]);
+                m.append(&mut data);
+                AndroidAutoFrame {
+                    header: FrameHeader {
+                        channel_id: chan,
+                        frame: FrameHeaderContents::new(true, FrameHeaderType::Single, false),
+                    },
+                    data: m,
+                }
+            }
+            SensorMessage::Event(chan, m) => {
+                let mut data = m.write_to_bytes().unwrap();
+                let t = Wifi::sensor_channel_message::Enum::SENSOR_EVENT_INDICATION as u16;
+                let t = t.to_be_bytes();
+                let mut m = Vec::new();
+                m.push(t[0]);
+                m.push(t[1]);
+                m.append(&mut data);
+                AndroidAutoFrame {
+                    header: FrameHeader {
+                        channel_id: chan,
+                        frame: FrameHeaderContents::new(true, FrameHeaderType::Single, false),
+                    },
+                    data: m,
+                }
+            }
+        }
+    }
+}
+
+impl TryFrom<&AndroidAutoFrame> for SensorMessage {
+    type Error = String;
+    fn try_from(value: &AndroidAutoFrame) -> Result<Self, Self::Error> {
+        use protobuf::Enum;
+        let mut ty = [0u8; 2];
+        ty.copy_from_slice(&value.data[0..2]);
+        let ty = u16::from_be_bytes(ty);
+        if let Some(sys) = Wifi::sensor_channel_message::Enum::from_i32(ty as i32) {
+            match sys {
+                Wifi::sensor_channel_message::Enum::SENSOR_START_REQUEST => {
+                    let m = Wifi::SensorStartRequestMessage::parse_from_bytes(&value.data[2..]);
+                    match m {
+                        Ok(m) => Ok(Self::SensorStartRequest(value.header.channel_id, m)),
+                        Err(e) => Err(e.to_string()),
+                    }
+                }
+                Wifi::sensor_channel_message::Enum::SENSOR_START_RESPONSE => unimplemented!(),
+                Wifi::sensor_channel_message::Enum::SENSOR_EVENT_INDICATION => unimplemented!(),
+                Wifi::sensor_channel_message::Enum::NONE => unimplemented!(),
+            }
+        } else {
+            Err(format!("Not converted message: {:x?}", value.data))
+        }
+    }
+}
+
+/// The handler for the sensor channel in the android auto protocol.
+pub struct SensorChannelHandler {}
+
+impl ChannelHandlerTrait for SensorChannelHandler {
+    fn build_channel<T: AndroidAutoMainTrait + ?Sized>(
+        &self,
+        _config: &AndroidAutoConfiguration,
+        chanid: ChannelId,
+        main: &T,
+    ) -> Option<Wifi::ChannelDescriptor> {
+        let mut chan = ChannelDescriptor::new();
+        let mut sensor = Wifi::SensorChannel::new();
+        let s = main.get_supported_sensors();
+        for s in &s.sensors {
+            sensor.sensors.push({
+                let mut sensor1 = Wifi::Sensor::new();
+                sensor1.set_type(*s);
+                sensor1
+            });
+        }
+        chan.sensor_channel.0.replace(Box::new(sensor));
+        chan.set_channel_id(chanid as u32);
+        if !chan.is_initialized() {
+            panic!("Channel not initialized?");
+        }
+        Some(chan)
+    }
+
+    async fn receive_data<
+        T: super::AndroidAutoMainTrait + ?Sized,
+        U: tokio::io::AsyncRead + Unpin,
+        V: tokio::io::AsyncWrite + Unpin,
+    >(
+        &self,
+        msg: AndroidAutoFrame,
+        stream: &StreamMux<U, V>,
+        _config: &AndroidAutoConfiguration,
+        main: &T,
+    ) -> Result<(), super::FrameIoError> {
+        let channel = msg.header.channel_id;
+        let msg2: Result<SensorMessage, String> = (&msg).try_into();
+        if let Ok(msg2) = msg2 {
+            match msg2 {
+                SensorMessage::Event(_chan, _m) => unimplemented!(),
+                SensorMessage::SensorStartResponse(_, _) => unimplemented!(),
+                SensorMessage::SensorStartRequest(_chan, m) => {
+                    let mut m2 = Wifi::SensorStartResponseMessage::new();
+
+                    let stat = match main.start_sensor(m.sensor_type()).await {
+                        Ok(_) => Wifi::status::Enum::OK,
+                        Err(_) => Wifi::status::Enum::FAIL,
+                    };
+                    m2.set_status(stat);
+                    stream
+                        .write_frame(SensorMessage::SensorStartResponse(channel, m2).into())
+                        .await?;
+                }
+            }
+            return Ok(());
+        }
+        let msg2: Result<AndroidAutoCommonMessage, String> = (&msg).try_into();
+        if let Ok(msg2) = msg2 {
+            match msg2 {
+                AndroidAutoCommonMessage::ChannelOpenResponse(_, _) => unimplemented!(),
+                AndroidAutoCommonMessage::ChannelOpenRequest(_m) => {
+                    let mut m2 = Wifi::ChannelOpenResponse::new();
+                    m2.set_status(Wifi::status::Enum::OK);
+                    stream
+                        .write_frame(
+                            AndroidAutoCommonMessage::ChannelOpenResponse(channel, m2).into(),
+                        )
+                        .await?;
+                }
+            }
+            return Ok(());
+        }
+        todo!("{:x?}", msg);
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,8 @@
+use android_auto::{
+    AndroidAutoAudioInputTrait, AndroidAutoAudioOutputTrait, AndroidAutoInputChannelTrait,
+    AndroidAutoMainTrait, AndroidAutoSensorTrait, AndroidAutoVideoChannelTrait,
+    AndroidAutoWiredTrait, VideoConfiguration,
+};
 use axum::{
     extract::{
         ws::{Message as WsMessage, WebSocket, WebSocketUpgrade},
@@ -9,125 +14,256 @@ use axum::{
 };
 use futures::{sink::SinkExt, stream::StreamExt};
 use serde::{Deserialize, Serialize};
-use std::{net::SocketAddr, sync::Arc};
-use tokio::sync::{broadcast, mpsc};
+use std::{collections::HashSet, net::SocketAddr, sync::Arc};
+use tokio::sync::{broadcast, mpsc, Mutex};
 use tower_http::services::ServeDir;
 use tracing::{error, info};
 
-// Assuming the `android-auto` crate provides channels logic similar to this:
-// use android_auto::usb::UsbConnection;
-// use android_auto::channels::{VideoChannel, InputChannel};
-
-/// Android Auto protocol defines actions for touch events.
 #[derive(Clone, Debug, Serialize, Deserialize)]
 pub struct TouchEvent {
-    pub action: u8, // Typically 0 = down, 1 = up, 2 = move
+    pub action: u8,
     pub x: u32,
     pub y: u32,
 }
 
 struct AppState {
-    /// Broadcast channel for H.264 NAL units coming from Android Auto UI Video Channel
     video_tx: broadcast::Sender<bytes::Bytes>,
-    /// MPSC channel to send touch events back to Android Auto Input Channel
     touch_tx: mpsc::Sender<TouchEvent>,
+}
+
+#[derive(Clone)]
+struct AppHeadunit {
+    video_tx: broadcast::Sender<bytes::Bytes>,
+    android_send: mpsc::Sender<android_auto::SendableAndroidAutoMessage>,
+    android_recv: Arc<Mutex<Option<mpsc::Receiver<android_auto::SendableAndroidAutoMessage>>>>,
+    config: VideoConfiguration,
+    sensors: android_auto::SensorInformation,
+    input_config: android_auto::InputConfiguration,
+}
+
+#[async_trait::async_trait]
+impl AndroidAutoVideoChannelTrait for AppHeadunit {
+    async fn receive_video(&self, data: Vec<u8>, _timestamp: Option<u64>) {
+        // Zero-copy broadcast to all Web UI Clients
+        let _ = self.video_tx.send(bytes::Bytes::from(data));
+    }
+    async fn setup_video(&self) -> Result<(), ()> { Ok(()) }
+    async fn teardown_video(&self) {}
+    async fn wait_for_focus(&self) {}
+    async fn set_focus(&self, _focus: bool) {}
+    fn retrieve_video_configuration(&self) -> &VideoConfiguration {
+        &self.config
+    }
+}
+
+#[async_trait::async_trait]
+impl AndroidAutoAudioOutputTrait for AppHeadunit {
+    async fn open_output_channel(&self, _t: android_auto::AudioChannelType) -> Result<(), ()> { Ok(()) }
+    async fn close_output_channel(&self, _t: android_auto::AudioChannelType) -> Result<(), ()> { Ok(()) }
+    async fn receive_output_audio(&self, _t: android_auto::AudioChannelType, _data: Vec<u8>) {}
+    async fn start_output_audio(&self, _t: android_auto::AudioChannelType) {}
+    async fn stop_output_audio(&self, _t: android_auto::AudioChannelType) {}
+}
+
+#[async_trait::async_trait]
+impl AndroidAutoAudioInputTrait for AppHeadunit {
+    async fn open_input_channel(&self) -> Result<(), ()> { Ok(()) }
+    async fn close_input_channel(&self) -> Result<(), ()> { Ok(()) }
+    async fn start_input_audio(&self) {}
+    async fn audio_input_ack(&self, _chan: u8, _ack: android_auto::Wifi::AVMediaAckIndication) {}
+    async fn stop_input_audio(&self) {}
+}
+
+#[async_trait::async_trait]
+impl AndroidAutoSensorTrait for AppHeadunit {
+    fn get_supported_sensors(&self) -> &android_auto::SensorInformation {
+        &self.sensors
+    }
+    async fn start_sensor(&self, stype: android_auto::Wifi::sensor_type::Enum) -> Result<(), ()> {
+        if self.sensors.sensors.contains(&stype) {
+            let mut m3 = android_auto::Wifi::SensorEventIndication::new();
+            match stype {
+                android_auto::Wifi::sensor_type::Enum::DRIVING_STATUS => {
+                    let mut ds = android_auto::Wifi::DrivingStatus::new();
+                    ds.set_status(android_auto::Wifi::DrivingStatusEnum::UNRESTRICTED as i32);
+                    m3.driving_status.push(ds);
+                }
+                android_auto::Wifi::sensor_type::Enum::NIGHT_DATA => {
+                    let mut ds = android_auto::Wifi::NightMode::new();
+                    ds.set_is_night(false);
+                    m3.night_mode.push(ds);
+                }
+                _ => {}
+            }
+            let m = android_auto::AndroidAutoMessage::Sensor(m3);
+            let _ = self.android_send.send(m.sendable()).await;
+            Ok(())
+        } else {
+            Err(())
+        }
+    }
+}
+
+#[async_trait::async_trait]
+impl AndroidAutoInputChannelTrait for AppHeadunit {
+    async fn binding_request(&self, _code: u32) -> Result<(), ()> { Ok(()) }
+    fn retrieve_input_configuration(&self) -> &android_auto::InputConfiguration {
+        &self.input_config
+    }
+}
+
+#[cfg(feature = "usb")]
+#[async_trait::async_trait]
+impl AndroidAutoWiredTrait for AppHeadunit {}
+
+#[async_trait::async_trait]
+impl AndroidAutoMainTrait for AppHeadunit {
+    async fn connect(&self) {
+        info!("--- ANDROID AUTO USB CONNECTED ---");
+    }
+
+    async fn disconnect(&self) {
+        info!("--- ANDROID AUTO DISCONNECTED ---");
+    }
+
+    async fn get_receiver(
+        &self,
+    ) -> Option<tokio::sync::mpsc::Receiver<android_auto::SendableAndroidAutoMessage>> {
+        let mut inner = self.android_recv.lock().await;
+        inner.take()
+    }
+
+    #[cfg(feature = "usb")]
+    fn supports_wired(&self) -> Option<Arc<dyn AndroidAutoWiredTrait>> {
+        Some(Arc::new(self.clone()))
+    }
 }
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     tracing_subscriber::fmt::init();
-    info!("Starting Android Auto Web UI Bridge");
+    info!("Starting Android Auto Web UI Host on 0.0.0.0:8080");
 
-    // Channels bridging AA and WS clients
-    let (video_tx, _) = broadcast::channel(1024); // Large buffer for zero-copy H.264 frame drops management
+    let (video_tx, _) = broadcast::channel(1024);
     let (touch_tx, mut touch_rx) = mpsc::channel(128);
 
-    let state = Arc::new(AppState {
-        video_tx: video_tx.clone(),
-        touch_tx,
-    });
-
-    // 1. Spawn Android Auto Background Task
-    let video_tx_aa = video_tx.clone();
+    let (android_send, android_recv) = mpsc::channel(50);
+    
+    // 1. Thread for handling WS -> Android Auto Touch Translation
+    let android_send2 = android_send.clone();
     tokio::spawn(async move {
-        info!("Android Auto USB listener starting...");
-        
-        // --- BOILERPLATE FOR ANDROID-AUTO CRATE ---
-        // let mut aa = android_auto::usb::UsbConnection::connect().await.expect("Failed to bind USB");
-        // let mut video_channel = aa.open_video_channel();
-        // let mut input_channel = aa.open_input_channel();
-        
-        loop {
-            tokio::select! {
-                // Read video frames from AA and broadcast
-                // Some(frame) = video_channel.next() => {
-                //       Zero-copy conversion into bytes, ready for broadcast to all WS clients
-                //      let _ = video_tx_aa.send(bytes::Bytes::from(frame.payload));
-                // }
-                
-                // Read touch events from WS clients and send to AA
-                Some(touch_event) = touch_rx.recv() => {
-                     info!("Injecting touch event into AA: {:?}", touch_event);
-                     // Example of using the `android-auto` InputChannel to send touch:
-                     // input_channel.send_touch(touch_event.action, touch_event.x, touch_event.y).await.unwrap();
-                }
+        while let Some(touch) = touch_rx.recv().await {
+            let mut i_event = android_auto::Wifi::InputEventIndication::new();
+            let timestamp: u64 = std::time::SystemTime::now()
+                .duration_since(std::time::UNIX_EPOCH)
+                .unwrap()
+                .as_micros() as u64;
+            i_event.set_timestamp(timestamp);
+            let mut te = android_auto::Wifi::TouchEvent::new();
+            let mut tl = android_auto::Wifi::TouchLocation::new();
+            tl.set_x(touch.x);
+            tl.set_y(touch.y);
+            tl.set_pointer_id(0);
+            te.touch_location = vec![tl];
+
+            if touch.action == 0 {
+                te.set_touch_action(android_auto::Wifi::touch_action::Enum::POINTER_DOWN);
+            } else if touch.action == 1 {
+                te.set_touch_action(android_auto::Wifi::touch_action::Enum::POINTER_UP);
+            } else {
+                te.set_touch_action(android_auto::Wifi::touch_action::Enum::DRAG);
+            }
+
+            i_event.touch_event = android_auto::protobuf::MessageField::some(te);
+            let e = android_auto::AndroidAutoMessage::Input(i_event);
+            if let Err(e) = android_send2.send(e.sendable()).await {
+                 error!("Failed to inject touch event into AA: {:?}", e);
             }
         }
     });
 
-    // 2. Setup HTTP/WebSocket Server
+    let mut sensors = HashSet::new();
+    sensors.insert(android_auto::Wifi::sensor_type::Enum::DRIVING_STATUS);
+    sensors.insert(android_auto::Wifi::sensor_type::Enum::NIGHT_DATA);
+
+    let headunit = AppHeadunit {
+        video_tx: video_tx.clone(),
+        android_send,
+        android_recv: Arc::new(Mutex::new(Some(android_recv))),
+        config: VideoConfiguration {
+            resolution: android_auto::Wifi::video_resolution::Enum::_1080p,
+            fps: android_auto::Wifi::video_fps::Enum::_30,
+            dpi: 160,
+        },
+        sensors: android_auto::SensorInformation { sensors },
+        input_config: android_auto::InputConfiguration {
+            keycodes: vec![1, 2, 3, 4, 5],
+            touchscreen: Some((1920, 1080)),
+        },
+    };
+
+    // 2. Start Android Auto Logic via MainTrait Loop
+    tokio::task::spawn(async move {
+        let config = android_auto::AndroidAutoConfiguration {
+            make: "Debian".to_string(),
+            model: "VM Headunit".to_string(),
+            year: "2024".to_string(),
+        };
+        let setup = android_auto::AndroidAutoSetup {
+            id: [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15],
+            display: "Headunit Display".to_string(),
+            setup: Default::default(),
+        };
+
+        let mut joinset = tokio::task::JoinSet::new();
+        info!("Wait for USB Phone connection...");
+        let b = Box::new(headunit);
+        let _ = b.run(config, &mut joinset, &setup).await;
+        joinset.join_all().await;
+        info!("Android auto session task ended.");
+    });
+
+    // 3. Web UI Server
+    let state = Arc::new(AppState { video_tx, touch_tx });
     let app = Router::new()
-        // Serve the frontend HTML/JS files statically from "public"
         .nest_service("/", ServeDir::new("public"))
         .route("/ws", get(ws_handler))
         .with_state(state);
 
     let addr = SocketAddr::from(([0, 0, 0, 0], 8080));
     let listener = tokio::net::TcpListener::bind(&addr).await?;
-    info!("Listening on http://{}", addr);
-
     axum::serve(listener, app).await?;
+
     Ok(())
 }
 
-/// Upgrades the HTTP request to a WebSocket connection
 async fn ws_handler(ws: WebSocketUpgrade, State(state): State<Arc<AppState>>) -> impl IntoResponse {
     ws.on_upgrade(|socket| handle_socket(socket, state))
 }
 
-/// Handles the actual WebSocket duplex stream for a single Web UI client
 async fn handle_socket(socket: WebSocket, state: Arc<AppState>) {
     let (mut sender, mut receiver) = socket.split();
     let mut video_rx = state.video_tx.subscribe();
 
-    // Stream H.264 Frames to Client
     let mut send_task = tokio::spawn(async move {
         while let Ok(frame) = video_rx.recv().await {
-            // Forward raw H.264 binary frame via zero-copy Bytes payload
             if sender.send(WsMessage::Binary(frame.to_vec())).await.is_err() {
                 break;
             }
         }
     });
 
-    // Receive JSON Touch Events from Client
     let touch_tx = state.touch_tx.clone();
     let mut recv_task = tokio::spawn(async move {
         while let Some(Ok(WsMessage::Text(msg))) = receiver.next().await {
-            match serde_json::from_str::<TouchEvent>(&msg) {
-                Ok(touch_event) => {
-                    if touch_tx.send(touch_event).await.is_err() {
-                        break;
-                    }
-                }
-                Err(e) => {
-                    error!("Failed to parse touch event JSON: {}", e);
+            if let Ok(touch_event) = serde_json::from_str::<TouchEvent>(&msg) {
+                if touch_tx.send(touch_event).await.is_err() {
+                    break;
                 }
             }
         }
     });
 
-    // Automatically disconnect and cleanup if either channel dies
     tokio::select! {
         _ = (&mut send_task) => recv_task.abort(),
         _ = (&mut recv_task) => send_task.abort(),
